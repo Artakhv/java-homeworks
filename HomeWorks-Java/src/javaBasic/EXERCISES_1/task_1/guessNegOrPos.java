fe70a0732120eea495886a6d92a42870fea391c4
Artakhv <artak.hovhannisyan10@gmail.com>
2020-03-26 20:39:42 +0400

    Task 1

--- HomeWorks-Java/src/javaBasic/EXERCISES_1/task_1/guessNegOrPos.java
+++ HomeWorks-Java/src/javaBasic/EXERCISES_1/task_1/guessNegOrPos.java
@@ -1,4 +1,21 @@
 package javaBasic.EXERCISES_1.task_1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class guessNegOrPos {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bufferedReader.readLine());
+        String message;
+        if (num > 0) {
+            message = "positive";
+        } else if (num < 0) {
+            message = "negative";
+        } else {
+            message = "zero";
+        }
+        System.out.println(message);
+    }
 }
