971a59e758530bc0a5bf08c7a50fe51fbd559cce
Artakhv <artak.hovhannisyan10@gmail.com>
2020-03-28 11:54:21 +0400

    Add recommended wat

--- HomeWorks-Java/src/javaBasic/EXERCISES_1/task_7/CheckIfNumberPrime.java
+++ HomeWorks-Java/src/javaBasic/EXERCISES_1/task_7/CheckIfNumberPrime.java
@@ -17,4 +17,18 @@ public class CheckIfNumberPrime {
         }
         return true;
     }
+  /// recommended way
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for(int i = 2; i < Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
